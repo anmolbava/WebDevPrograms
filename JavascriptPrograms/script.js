8df458e3408000084dc30664193a464236a2310d
anmolbava <anmolbava96@gmail.com>
2024-02-11 21:06:38 +0000

    updating webdevprograms

--- JavascriptPrograms/script.js
+++ JavascriptPrograms/script.js
@@ -1,3 +1,17 @@
-console.log("This is console loggging statement");
-window.alert("alert  statment");
+//let heading = document.getElementById("heading1");
+// console.log("heading")
 
+//let headings =document.querySelector('heading-class');
+//console.log(headings);
+//console.dir(headings);
+let ele = document.getElementsByClassName(".thisisheading");
+console.log(ele);
+
+let identity = document.getElementById("myid");
+console.dir(identity);
+
+let firstquery = document.querySelector(".thisisheading");
+console.dir(firstquery);
+
+let ekto = document.querySelectorAll("#myid");
+console.dir(ekto);
